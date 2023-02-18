bcc3f46f70eb9ae25a85ab6545173fa93f24f704
Маша <114254835+probina192@users.noreply.github.com>
2023-02-18 13:45:37 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -49,20 +49,20 @@ public class Main{
         drob1.vivodDrobi();
 
         //создание дроби
-        System.out.println("Введите ЦЕЛЫЕ значения числителя и знаменателя:");
+        System.out.println("Введите целые значения числителя и знаменателя:");
         Drobi drob2 = new Drobi(in.nextInt(), in.nextInt());
         drob2.vivodDrobi();
 
 
         //сумма
-        System.out.println("Складываем как сигма - Введите ЦЕЛЫЕ значения числителя и знаменателя первой дроби:");
+        System.out.println("сложение - Введите целые значения числителя и знаменателя первой дроби:");
         Drobi drob3 = new Drobi(in.nextInt(), in.nextInt());
-        System.out.println("Введите ЦЕЛЫЕ значения числителя и знаменателя второй дроби:");
+        System.out.println("Введите целые значения числителя и знаменателя второй дроби:");
         Drobi drob4 = new Drobi(in.nextInt(), in.nextInt());
         drob3.sum(drob3.chislitel, drob3.znamenatel, drob4.chislitel, drob4.znamenatel);
 
         //сумма со значением по умолчанию
-        System.out.println("Складываем как сигма - Введите ЦЕЛЫЕ значения числителя и знаменателя первой дроби:");
+        System.out.println("Сложение - Введите целые значения числителя и знаменателя первой дроби:");
         Drobi drob31 = new Drobi(in.nextInt(), in.nextInt());
         Drobi drob41 = new Drobi();
         System.out.println("Значение по умолчанию: "+ drob41.chislitel + "/"+ drob41.znamenatel);
@@ -70,42 +70,42 @@ public class Main{
 
 
         //вычитание дробей
-        System.out.println("Вычитание - Введите ЦЕЛЫЕ значения числителя и знаменателя первой дроби:");
+        System.out.println("Вычитание - Введите целые значения числителя и знаменателя первой дроби:");
         Drobi drob5 = new Drobi(in.nextInt(), in.nextInt());
-        System.out.println("Введите ЦЕЛЫЕ значения числителя и знаменателя второй дроби:");
+        System.out.println("Введите целые значения числителя и знаменателя второй дроби:");
         Drobi drob6 = new Drobi(in.nextInt(), in.nextInt());
         drob5.min(drob5.chislitel, drob5.znamenatel, drob6.chislitel, drob6.znamenatel);
 
         //вычитанние дробей со значением по умолчанию
-        System.out.println("Вычитание - Введите ЦЕЛЫЕ значения числителя и знаменателя первой дроби:");
+        System.out.println("Вычитание - Введите целые значения числителя и знаменателя первой дроби:");
         Drobi drob51 = new Drobi(in.nextInt(), in.nextInt());
         Drobi drob61 = new Drobi();
         System.out.println("Значение по умолчанию: "+ drob61.chislitel + "/"+ drob61.znamenatel);
         drob5.min(drob51.chislitel, drob51.znamenatel, drob61.chislitel, drob61.znamenatel);
 
         //умножение
-        System.out.println("Умножение - Введите ЦЕЛЫЕ значения числителя и знаменателя первой дроби:");
+        System.out.println("Умножение - Введите целые значения числителя и знаменателя первой дроби:");
         Drobi drob7 = new Drobi(in.nextInt(), in.nextInt());
-        System.out.println("Введите ЦЕЛЫЕ значения числителя и знаменателя второй дроби:");
+        System.out.println("Введите целые значения числителя и знаменателя второй дроби:");
         Drobi drob8 = new Drobi(in.nextInt(), in.nextInt());
         drob7.umnogenie(drob7.chislitel, drob7.znamenatel, drob8.chislitel, drob8.znamenatel);
 
         //умножение со значением по умалчиванию
-        System.out.println("Умножение - Введите ЦЕЛЫЕ значения числителя и знаменателя первой дроби:");
+        System.out.println("Умножение - Введите целые значения числителя и знаменателя первой дроби:");
         Drobi drob71 = new Drobi(in.nextInt(), in.nextInt());
         Drobi drob81 = new Drobi();
         System.out.println("Значение по умолчанию: "+ drob81.chislitel + "/"+ drob81.znamenatel);
         drob7.umnogenie(drob71.chislitel, drob71.znamenatel, drob81.chislitel, drob81.znamenatel);
 
         //деление
-        System.out.println("Деление - Введите ЦЕЛЫЕ значения числителя и знаменателя первой дроби:");
+        System.out.println("Деление - Введите целые значения числителя и знаменателя первой дроби:");
         Drobi drob9 = new Drobi(in.nextInt(), in.nextInt());
-        System.out.println("Введите ЦЕЛЫЕ значения числителя и знаменателя второй дроби:");
+        System.out.println("Введите целые значения числителя и знаменателя второй дроби:");
         Drobi drob10 = new Drobi(in.nextInt(), in.nextInt());
         drob9.delenie(drob9.chislitel, drob9.znamenatel, drob10.chislitel, drob10.znamenatel);
 
         //деление со значениями по умолчанию
-        System.out.println("Деление - Введите ЦЕЛЫЕ значения числителя и знаменателя первой дроби:");
+        System.out.println("Деление - Введите целые значения числителя и знаменателя первой дроби:");
         Drobi drob91 = new Drobi(in.nextInt(), in.nextInt());
         Drobi drob101 = new Drobi();
         System.out.println("Значение по умолчанию: "+ drob91.chislitel + "/"+ drob101.znamenatel);
