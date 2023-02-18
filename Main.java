b41d9bded8d4b74036e1029e77086cb8a5f1e12e
Маша <114254835+probina192@users.noreply.github.com>
2023-02-18 13:41:50 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 class Drobi{
     public int chislitel, znamenatel;
 
-    //ДРОБЬ ПО УМАЛЧИВАНИЮ СЫС
+    //дробь по умолчанию
     public Drobi(){
         chislitel = 1;
         znamenatel = 1;
     }
-    //СОСДАЁТ ДРОПЬ
+    //создаем дробь
     public Drobi(int a, int b) throws Exception {
         if (b != 0) {
             chislitel = a;
             znamenatel = b;
         } else {
-            throw new Exception("На ноль делить- матан учить, а нам это не нужно; могу сказать, что lim(x/0)= +-бесконечность");
+            throw new Exception("знаменатель не равен 0");
 
 
         }
@@ -23,17 +23,17 @@ class Drobi{
     public void vivodDrobi(){
         System.out.println(chislitel + "/" + znamenatel);
     }
-    //СУММА В СУМКЕ У ПАПЫ
+    //сумма
     public void sum(int a, int b, int c, int d) {
         System.out.println("Результат: " + (a * d + b * c) + "/" + b * d);
     }
-    //НА БАРЕ СИНИЕ МЫ ТАНЦУЕМ ПОД МИНУС
+    //разность
     public void min(int a, int b, int c, int d) {System.out.println("Результат: " + (a * d - b * c) + "/" + b * d);}
-    //ДВАЖДЫ ДВА ЧОТЫРЕ ЭТО ВСЕМ ИЗВЕТНО В ЦЕЛОМ МИРЕ
+    //умножение
     public void umnogenie(int a, int b, int c, int d) {
         System.out.println("Результат: " + (a * c) + "/" + b * d);
     }
-    //ОТОБРАТЬ И ПОДЕЛИТЬ НАРОДНОЕ ДОСТОЯНИЕ CUMУНИЗМ РУЛИТ
+    //деление
     public void delenie(int a, int b, int c, int d) {
         System.out.println("Результат: " + (a * d) + "/" + c * b);
     }
@@ -44,24 +44,24 @@ public class Main{
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
 
-        //ДРОООООБЬ ПО УМАЛЧИВАНИЮ
+        //дробь по умолчанию
         Drobi drob1 = new Drobi();
         drob1.vivodDrobi();
 
-        //СОЗДАНИЕ ВСЕЛЕНСКОЙ ДРОБИ, КОТОРАЯ УЖЕ ТЫСЯЧИ И МИЛИЛОНЫ МИЛИАРДДОВ ЛЕТ ПРОЖИВАЕТ НА ЭТОЙ И ТРИЛЛИОНАХ ТАКИХ ЖЕ ГАЛАКТИК ЕЙ ЭТОТ МИР АБСОЛЮТНО ПОНЯТЕН
+        //создание дроби
         System.out.println("Введите ЦЕЛЫЕ значения числителя и знаменателя:");
         Drobi drob2 = new Drobi(in.nextInt(), in.nextInt());
         drob2.vivodDrobi();
 
 
-        //СКЛАДЫВАНИЕ
+        //сумма
         System.out.println("Складываем как сигма - Введите ЦЕЛЫЕ значения числителя и знаменателя первой дроби:");
         Drobi drob3 = new Drobi(in.nextInt(), in.nextInt());
         System.out.println("Введите ЦЕЛЫЕ значения числителя и знаменателя второй дроби:");
         Drobi drob4 = new Drobi(in.nextInt(), in.nextInt());
         drob3.sum(drob3.chislitel, drob3.znamenatel, drob4.chislitel, drob4.znamenatel);
 
-        //Складывание со значением по умолчанию
+        //сумма со значением по умолчанию
         System.out.println("Складываем как сигма - Введите ЦЕЛЫЕ значения числителя и знаменателя первой дроби:");
         Drobi drob31 = new Drobi(in.nextInt(), in.nextInt());
         Drobi drob41 = new Drobi();
@@ -69,48 +69,48 @@ public class Main{
         drob3.sum(drob31.chislitel, drob31.znamenatel, drob41.chislitel, drob41.znamenatel);
 
 
-        //Вычетаем дробби
+        //вычитание дробей
         System.out.println("Вычитание - Введите ЦЕЛЫЕ значения числителя и знаменателя первой дроби:");
         Drobi drob5 = new Drobi(in.nextInt(), in.nextInt());
         System.out.println("Введите ЦЕЛЫЕ значения числителя и знаменателя второй дроби:");
         Drobi drob6 = new Drobi(in.nextInt(), in.nextInt());
         drob5.min(drob5.chislitel, drob5.znamenatel, drob6.chislitel, drob6.znamenatel);
 
-        //Вычетаем дробби со значением по умолчанию
+        //вычитанние дробей со значением по умолчанию
         System.out.println("Вычитание - Введите ЦЕЛЫЕ значения числителя и знаменателя первой дроби:");
         Drobi drob51 = new Drobi(in.nextInt(), in.nextInt());
         Drobi drob61 = new Drobi();
         System.out.println("Значение по умолчанию: "+ drob61.chislitel + "/"+ drob61.znamenatel);
         drob5.min(drob51.chislitel, drob51.znamenatel, drob61.chislitel, drob61.znamenatel);
 
-        //ДЮП В МАЙНКРАФТЕ(УМНОЖЕНИЕ)
+        //умножение
         System.out.println("Умножение - Введите ЦЕЛЫЕ значения числителя и знаменателя первой дроби:");
         Drobi drob7 = new Drobi(in.nextInt(), in.nextInt());
         System.out.println("Введите ЦЕЛЫЕ значения числителя и знаменателя второй дроби:");
         Drobi drob8 = new Drobi(in.nextInt(), in.nextInt());
         drob7.umnogenie(drob7.chislitel, drob7.znamenatel, drob8.chislitel, drob8.znamenatel);
 
-        //ДЮП В МАЙНКРАФТЕ(УМНОЖЕНИЕ) Со значением по умалчиванию
+        //умножение со значением по умалчиванию
         System.out.println("Умножение - Введите ЦЕЛЫЕ значения числителя и знаменателя первой дроби:");
         Drobi drob71 = new Drobi(in.nextInt(), in.nextInt());
         Drobi drob81 = new Drobi();
         System.out.println("Значение по умолчанию: "+ drob81.chislitel + "/"+ drob81.znamenatel);
         drob7.umnogenie(drob71.chislitel, drob71.znamenatel, drob81.chislitel, drob81.znamenatel);
 
-        //БЫЛО ДВА ЯБЛОКА ИХ РАЗДЕЛИЛИ КАК ПОЛЬШУ В 39
+        //деление
         System.out.println("Деление - Введите ЦЕЛЫЕ значения числителя и знаменателя первой дроби:");
         Drobi drob9 = new Drobi(in.nextInt(), in.nextInt());
         System.out.println("Введите ЦЕЛЫЕ значения числителя и знаменателя второй дроби:");
         Drobi drob10 = new Drobi(in.nextInt(), in.nextInt());
         drob9.delenie(drob9.chislitel, drob9.znamenatel, drob10.chislitel, drob10.znamenatel);
 
-        //БЫЛО ДВА ЯБЛОКА ИХ РАЗДЕЛИЛИ КАК ПОЛЬШУ В 39 со значение по умолчанию(пакт молотова-риббентропа)
+        //деление со значениями по умолчанию
         System.out.println("Деление - Введите ЦЕЛЫЕ значения числителя и знаменателя первой дроби:");
         Drobi drob91 = new Drobi(in.nextInt(), in.nextInt());
         Drobi drob101 = new Drobi();
         System.out.println("Значение по умолчанию: "+ drob91.chislitel + "/"+ drob101.znamenatel);
         drob9.delenie(drob91.chislitel, drob91.znamenatel, drob101.chislitel, drob101.znamenatel);
-
+        in.close()
 
     }
     }
